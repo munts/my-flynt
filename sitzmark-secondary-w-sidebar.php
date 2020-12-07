d03efd16558ebae44292294ccfbe5480ccb98a73
Scott Taylor <s.taylor06@gmail.com>
2020-12-07 11:45:04 -0700

    update to the maps & directions link

--- sitzmark-secondary-w-sidebar.php
+++ sitzmark-secondary-w-sidebar.php
@@ -12,4 +12,7 @@ $dynamicSidebar = get_field('sidebar_to_use');
 $context = Timber::get_context();
 $context['post'] = new Post();
 $context['dynamic_sidebar'] = Timber::get_widgets($dynamicSidebar);
+$context['dynamic_sidebar1'] = Timber::get_widgets('footer_col_one');
+$context['dynamic_sidebar2'] = Timber::get_widgets('footer_col_two');
+$context['dynamic_sidebar3'] = Timber::get_widgets('footer_col_three');
 Timber::render('templates/secondary-page-w-sidebar.twig', $context);
