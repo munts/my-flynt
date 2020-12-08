15c7113a351417fec1831805d465fb4ecd903789
Scott Taylor <s.taylor06@gmail.com>
2020-12-08 14:03:56 -0700

    clean up work

--- Components/PremiumComponents/BlockCustomHtml/script.js
+++ Components/PremiumComponents/BlockCustomHtml/script.js
@@ -2,7 +2,3 @@ import { $, jQuery } from 'jquery'
 // export for others scripts to use
 window.$ = $
 window.jQuery = jQuery
-
-function doADocDotWrite () { // eslint-disable-line no-unused-vars
-  document.write('This is a warning message!')
-}
