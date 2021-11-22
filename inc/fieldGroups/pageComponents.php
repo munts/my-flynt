7bd8c66ed14baf7b5646c1ce27933407c0467913
Scott Taylor <s.taylor06@gmail.com>
2021-11-22 10:51:00 -0700

    booking engine link for mobile book now button

--- inc/fieldGroups/pageComponents.php
+++ inc/fieldGroups/pageComponents.php
@@ -23,6 +23,7 @@ add_action('Flynt/afterRegisterComponents', function () {
                     Components\BlockWysiwyg\getACFLayout(),
                     Components\GridImageText\getACFLayout(),
                     Components\GridPostsLatest\getACFLayout(),
+                    Components\GridPostsStaff\getACFLayout(),
                     Components\ListComponents\getACFLayout(),
                     Components\SliderImages\getACFLayout(),
                 ]
